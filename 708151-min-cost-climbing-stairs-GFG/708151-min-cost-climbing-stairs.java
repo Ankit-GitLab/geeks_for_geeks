d2f329095ad9cac85964afb56c300360f719048d
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-01 13:32:34 +0530

    Added solution - LeetHub

--- 708151-min-cost-climbing-stairs-GFG/708151-min-cost-climbing-stairs.java
+++ 708151-min-cost-climbing-stairs-GFG/708151-min-cost-climbing-stairs.java
@@ -1,18 +1,29 @@
 //Back-end complete function Template for Java
 
 class Solution {
-    static int[] dp;
     static int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        dp = new int[n];
-        Arrays.fill(dp,-1);
-        minCost(0,cost);
-        return Math.min(dp[0], dp[1]);
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<n; i++){
+            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-2], dp[n-1]);
     }
+    
+    // static int[] dp;
+    // static int minCostClimbingStairs(int[] cost) {
+    //     int n = cost.length;
+    //     dp = new int[n];
+    //     Arrays.fill(dp,-1);
+    //     minCost(0,cost);
+    //     return Math.min(dp[0], dp[1]);
+    // }
 
-    private static int minCost(int i, int[] cost) {
-        if(i>=cost.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = cost[i] + Math.min(minCost(i+1,cost),minCost(i+2,cost));
-    }
+    // private static int minCost(int i, int[] cost) {
+    //     if(i>=cost.length) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     return dp[i] = cost[i] + Math.min(minCost(i+1,cost),minCost(i+2,cost));
+    // }
 };
