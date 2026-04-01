094f5ea050e55dd38ed7f679bb0805310dcc7253
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-01 12:06:26 +0530

    Added solution - LeetHub

---
+++ 706436-flatten-binary-tree-to-linked-list-GFG/706436-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+       public static void flatten(Node root){
+        if(root ==null) return;
+        Node lst = root.left;
+        Node rst = root.right;
+        root.left = null;
+
+        flatten(lst);
+        flatten(rst);
+
+        root.right = lst;
+        Node last = root;
+        while(last.right != null) last = last.right;
+        last.right = rst;
+
+    }
+}
