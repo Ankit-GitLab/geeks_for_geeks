5a8af95c56105154ee607ed3bdfbf407c8353193
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-03 09:38:19 +0530

    Added solution - LeetHub

--- 703687-friends-pairing-problem-GFG/703687-friends-pairing-problem.java
+++ 703687-friends-pairing-problem-GFG/703687-friends-pairing-problem.java
@@ -2,16 +2,32 @@
 
 class Solution {
     public long countFriendsPairings(int n) {
-        long[] dp = new long[n+1];
-        return friends(n,dp);
+    if(n <= 2) return n;
+
+    long prev2 = 1; // f(1)
+    long prev1 = 2; // f(2)
+
+    for(int i = 3; i <= n; i++){
+        long curr = prev1 + (i - 1) * prev2;
+        prev2 = prev1;
+        prev1 = curr;
     }
 
-    private long friends(int n, long[] dp) {
-        if(n<=2) return n;
-        if(dp[n]!=0) return dp[n];
-        return dp[n] = countFriendsPairings(n-1) + (n-1)*countFriendsPairings(n-2);
+    return prev1;
+}
+    
+    
+    // public long countFriendsPairings(int n) {
+    //     long[] dp = new long[n+1];
+    //     return friends(n,dp);
+    // }
 
-    }
+    // private long friends(int n, long[] dp) {
+    //     if(n<=2) return n;
+    //     if(dp[n]!=0) return dp[n];
+    //     return dp[n] = countFriendsPairings(n-1) + (n-1)*countFriendsPairings(n-2);
+
+    // }
     
     
     //  public long countFriendsPairings(int n) {
